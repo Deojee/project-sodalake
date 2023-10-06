ac2123297269037fa4d8c7b9af574711e5415dda
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2023-10-06 12:13:23 -0400

    added a silly player

---
+++ Player.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if Input.is_action_pressed("ui_left"):
+		position.x -= 10
+	if Input.is_action_pressed("ui_right"):
+		position.x += 10
+	if Input.is_action_pressed("ui_up"):
+		position.y -= 10
+	if Input.is_action_pressed("ui_down"):
+		position.y += 10
+		
+	pass
