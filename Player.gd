b7e25c36355d527b50762dc2072edcbb47d615b9
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2023-10-06 14:10:15 -0400

    it works!

--- Player.gd
+++ Player.gd
@@ -5,17 +5,22 @@ extends Node2D
 func _ready():
 	pass # Replace with function body.
 
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+	$Camera2D.enabled = is_multiplayer_authority()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
-	if Input.is_action_pressed("ui_left"):
-		position.x -= 10
-	if Input.is_action_pressed("ui_right"):
-		position.x += 10
-	if Input.is_action_pressed("ui_up"):
-		position.y -= 10
-	if Input.is_action_pressed("ui_down"):
-		position.y += 10
+	if is_multiplayer_authority():
+		if Input.is_action_pressed("ui_left"):
+			position.x -= 10
+		if Input.is_action_pressed("ui_right"):
+			position.x += 10
+		if Input.is_action_pressed("ui_up"):
+			position.y -= 10
+		if Input.is_action_pressed("ui_down"):
+			position.y += 10
 		
 	pass
--- world.gd
+++ world.gd
@@ -1,25 +1,77 @@
 extends Node2D
 
-@export var isServer = true
+var peer 
+@export var player_scene : PackedScene
+@onready var cam = $Camera2D
+var port = 8910
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
-	# Create server.
-	if isServer:
-		var peer = ENetMultiplayerPeer.new()
-		peer.create_server(0, 10)
-		multiplayer.multiplayer_peer = peer
+	multiplayer.connected_to_server.connect(connectedToServer)
+	multiplayer.connection_failed.connect(failedToConnect)
+	pass # Replace with function body.
+
+func connectedToServer():
+	print("connected!")
+func failedToConnect():
+	print("failed")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var temp = ""
 	
-	else:
-		var peer = ENetMultiplayerPeer.new()
-		peer.create_client("136.228.38.11", 0)
-		multiplayer.multiplayer_peer = peer
+	temp += str(multiplayer.get_default_interface()) + "\n"
+	
+	temp += str(multiplayer.get_peers()) + "\n"
+	
+	$Label.text = temp
+	
+	
+	pass
+
+
+func _on_host_button_pressed():
+	peer = ENetMultiplayerPeer.new()
+	
+	var error = peer.create_server(port,4)
+	
+	print(error)
+	
+	multiplayer.set_multiplayer_peer(peer)
+	multiplayer.peer_connected.connect(add_player)
+	add_player()
+	cam.enabled = false
+	
+	print("waiting for players!")
 	
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if (Input.is_action_just_pressed("ui_accept")):
-		$subWorld.add_child(load("res://player.tscn").instantiate())
-	pass
+func _on_client_button_pressed():
+	peer = ENetMultiplayerPeer.new()
+	
+	var err = peer.create_client("10.99.3.18",port)
+	multiplayer.multiplayer_peer = peer
+	cam.enabled = false
+	
+	print(err)
+	
+	pass # Replace with function body.
+
+
+func exit_game(id):
+	multiplayer.peer_disconnected.connect(del_player)
+	del_player(id)
+
+func add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child",player)
+	
+
+func del_player(id):
+	rpc("_del_player",id)
+@rpc("any_peer", "call_local") func _del_player(id):
+	get_node(str(id)).queue_free()
+
