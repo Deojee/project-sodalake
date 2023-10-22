3ffd509da451221e4bf05918d7fbafae6ab5881c
jonathan-quin <112405329+jonathan-quin@users.noreply.github.com>
2023-10-21 21:38:38 -0400

    added more name censoring. started on tilemaps

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -66,7 +66,25 @@ func censorSwears(realName: String) -> String:
 		for i in len(swear):
 			temp += "#"
 		censoredName = censoredName.replacen(swear, temp )
-	return censoredName
+	
+	var legalCharacters = [
+		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+		"N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 
+		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
+		"n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", 
+		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "_","-"
+	]
+  # Replace with actual swears
+	
+	var newName = ""
+	for i in range(censoredName.length()):
+		var current_char = censoredName[i]
+		if legalCharacters.has(current_char):
+			newName += current_char
+		
+		
+	
+	return newName
 
 
 
